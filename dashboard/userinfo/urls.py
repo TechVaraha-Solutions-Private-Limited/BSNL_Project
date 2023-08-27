215a553f08d85da1fec4daa366571c85ceaddee7
Vickymj <barathvignesh25@gmail.com>
2023-08-27 16:29:01 +0530

    login function completed

--- dashboard/userinfo/urls.py
+++ dashboard/userinfo/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import add_role, add_user, add_role_group
+from .views import admin_login, add_role, add_user, add_role_group, logout_admin_user
 
 
 urlpatterns = [
-    path('', add_user, name='add_user'),
+    path('', admin_login, name='admin_login'),
+    path('add-user', add_user, name='add_user'),
     path('add-role',add_role, name='add_role'),
     path('add-role-group',add_role_group, name='add_role_group'),
+    path('logout',logout_admin_user, name='logout_admin_user'),
 ]
