5ce1b9f9870dd3ff29f3a669656f4ec47b48c152
Muthusamyc <119847186+Muthusamyc@users.noreply.github.com>
2023-10-31 16:26:18 +0530

    receipt print changes

--- dashboard/reports/views.py
+++ dashboard/reports/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from dashboard.members.models import Bookings,PaymentDetails
 from dashboard.userinfo.models import UserDetail
+from num2words import num2words
 # Create your views here.
 def confirmletter_view(request,id):
     book=Bookings.objects.get(id=id)
@@ -11,8 +12,27 @@ def confirmletter_view(request,id):
 def print_recepit(request,id):
     user=Bookings.objects.get(id=id)
     userdetail = UserDetail.objects.get(user=user.user)
-
     payments = PaymentDetails.objects.filter(booking_id=user.id)
+    payment_count = PaymentDetails.objects.filter(booking_id=user.id).values('receipt_no').distinct().count()
+    print(payment_count)
     for payment in payments:
-        print(payment.id)
-    return render (request,'print_recepit.html',{'user':user,'userdetail':userdetail,'payment':payments})
+        last_payment = payment.amount
+        if last_payment:
+            no = float(last_payment)
+            receipt_no = num2words(no)
+            print(receipt_no)
+    get_last = PaymentDetails.objects.filter(booking_id=user.id).last()
+    value = get_last.amount
+    no = float(value)
+    word1 = num2words(no, lang='en_IN')
+    word = word1.replace(',','')
+    print("132545",word)   
+    context ={
+        'user':user,
+        'userdetail':userdetail,
+        'payment':payments,
+        'payment_count':payment_count,
+        'word':word,
+        'value':value,
+    }
+    return render (request,'print_recepit.html',context)
