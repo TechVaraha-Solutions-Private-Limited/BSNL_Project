d141f4c814ee78b1e5667c091b1ee968526b1074
Muthusamyc <119847186+Muthusamyc@users.noreply.github.com>
2023-11-04 15:52:54 +0530

    report date update

--- dashboard/reports/views.py
+++ dashboard/reports/views.py
@@ -4,6 +4,7 @@ from dashboard.projects.models import Project
 from dashboard.userinfo.models import UserDetail
 from num2words import num2words
 from django.db.models import Sum
+from datetime import datetime, date, timedelta
 # Create your views here.
 def confirmletter_view(request,id):
     book=Bookings.objects.get(id=id)
@@ -43,31 +44,57 @@ def booking_report(request):
     view_report = PaymentDetails.objects.all()
     project = Project.objects.all()
     for view in view_report:
-        detail = UserDetail.objects.get(user_id = view.booking.user)
+        detail = UserDetail.objects .get(user_id = view.booking.user)
         view.userinfo = detail.alternate_no
         view.address = detail.address
         #print(view.booking.land_details.project.projectname)
     if request.method == 'POST':
         value = request.POST.get('projectOption')
-        view_reports = PaymentDetails.objects.distinct()
-        bookings = Bookings.objects.filter(land_details__project__projectname = value)
+        select = request.POST.get('reportType')
+        print(select)
+        fromDate = request.POST.get('fromDate','')
+        
         #filter(booking__land_details__project__projectname=value)
-        print(value)
-        if value == 'all':
-                print(value)
-                bookings = Bookings.objects.all() 
-                print(bookings)
-        for booking in bookings:
+        if select == 'project':
             
-            payments = booking.paymentdetails_set.all()
-            booking.total_amt = payments.aggregate(Sum('amount'))['amount__sum']
-            booking.address = UserDetail.objects.get(user_id = booking.user.id).address
-            booking.alter = UserDetail.objects.get(user_id = booking.user.id).alternate_no
-           
+            if value == 'all':
+                bookings = Bookings.objects.all()
+            else:
+                bookings = Bookings.objects.filter(land_details__project__projectname = value)
+            for booking in bookings:
+                payments = booking.paymentdetails_set.all()
+                booking.total_amt = payments.aggregate(Sum('amount'))['amount__sum']
+                booking.address = UserDetail.objects.get(user_id = booking.user.id).address
+                booking.alter = UserDetail.objects.get(user_id = booking.user.id).alternate_no
+        elif select == 'date':
+            fromDate = request.POST.get('fromDate','')
+            toDate = request.POST.get('toDate','')
+            if toDate and fromDate:
+                date_obj = datetime.strptime(toDate, '%Y-%m-%d')
+                fromDate = datetime.strptime(fromDate, '%Y-%m-%d')
+                print(fromDate)
+                date_obj = datetime.strptime(toDate, '%Y-%m-%d')
+                toDate =  date_obj + timedelta(days=1)
+                bookings = Bookings.objects.filter(created_on__gte=date(fromDate.year, fromDate.month, fromDate.day),
+                                                   created_on__lte=date(date_obj.year, date_obj.month, date_obj.day)).all()
+                for booking in bookings:
+                    payments = booking.paymentdetails_set.all()
+                    booking.total_amt = payments.aggregate(Sum('amount'))['amount__sum']
+                    booking.address = UserDetail.objects.get(user_id = booking.user.id).address
+                    booking.alter = UserDetail.objects.get(user_id = booking.user.id).alternate_no
+        else:
+            bookings = Bookings.objects.all()
+            
+            for booking in bookings:
+                payments = booking.paymentdetails_set.all()
+                booking.total_amt = payments.aggregate(Sum('amount'))['amount__sum']
+                booking.address = UserDetail.objects.get(user_id = booking.user.id).address
+                booking.alter = UserDetail.objects.get(user_id = booking.user.id).alternate_no
+
+       
         context={
         'view_report': view_report,
         'project':project,
-        'view_reports':view_reports,
         'bookings':bookings,
         }
         return render(request, 'booking_report.html', context)
